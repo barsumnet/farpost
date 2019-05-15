380f77a1100607cd9788f43cadd3f8808c8af111
bee-r <admin@barum.net>
2019-05-16 01:20:35 +1000

    Initial commit

---
+++ src/Farpost.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Beer\Farpost;
+
+/**
+ * Description of Farpost
+ *
+ * @author User
+ */
+class Farpost {
+
+    public function test() {
+        return TRUE;
+    }
+
+}
